0731f2a3be63f3f0056e02c86516a78d7a320656
admin <admin@Albert-Khomich.local>
2024-03-14 17:55:14 +0100

    animations

--- js/main.js
+++ js/main.js
@@ -33,3 +33,57 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  animateAbout();
+  animateElements();
+});
+
+window.addEventListener("scroll", function () {
+  animateAbout();
+  animateElements();
+});
+
+function animateAbout() {
+  const placeholdersAbout = document.querySelectorAll(".img-placeholder");
+
+  placeholdersAbout.forEach(function (placeholder, index) {
+    const img = document.createElement("img");
+    img.src = placeholder.getAttribute("data-src");
+    img.alt = "Design Kuhn";
+    img.classList.add("img-fluid");
+    img.classList.add("about-img");
+    if (isInViewport(placeholder)) {
+      img.classList.add("about-animate-grow");
+      placeholder.parentNode.insertBefore(img, placeholder.nextSibling);
+      setTimeout(function () {
+        img.style.opacity = "1";
+      }, 500);
+      placeholder.remove();
+    }
+  });
+}
+
+function animateElements() {
+  const elements = document.querySelectorAll(".animated-text");
+
+  elements.forEach(function (elem) {
+    const delay = parseFloat(elem.getAttribute("data-delay") || "0") * 1000;
+    if (isInViewport(elem)) {
+      setTimeout(function () {
+        elem.classList.add("animate");
+      }, delay);
+    }
+  });
+}
+
+function isInViewport(elem) {
+  const rect = elem.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
