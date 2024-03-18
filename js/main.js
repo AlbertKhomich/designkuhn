6c096f0a0aba1ea95c2bfabbcacd164cf20c2240
admin <admin@Albert-Khomich.local>
2024-03-18 16:38:04 +0100

    front-end

--- js/main.js
+++ js/main.js
@@ -3,34 +3,112 @@
 document.body.classList.add("loaded");
 
 document.addEventListener("DOMContentLoaded", function () {
+  const rotatingImages = document.querySelectorAll(".rotate-15");
+
+  function setBigEllipseDimensions() {
+    const bigEllipseContainers = document.querySelectorAll(
+      ".elliptical-link-container-big"
+    );
+    bigEllipseContainers.forEach((container) => {
+      const image = container.querySelector(".elliptical-link");
+      const ellipse = container.querySelector(".ellipse-big");
+
+      const imageWidth = image.offsetWidth;
+
+      const ellipseWidth = imageWidth * 1.42;
+      const ellipseHeight = ellipseWidth * 0.45;
+
+      ellipse.style.width = `${ellipseWidth}px`;
+      ellipse.style.height = `${ellipseHeight}px`;
+    });
+  }
+
+  setBigEllipseDimensions();
+
+  window.addEventListener("resize", setBigEllipseDimensions);
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  rotatingImages.forEach((img) => {
+    observer.observe(img);
+  });
+
+  function handleIntersect(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        rotateImage(entry.target);
+      }
+    });
+  }
+
+  function rotateImage(image) {
+    window.addEventListener("scroll", function () {
+      const windowHeight = window.innerHeight;
+      const windowMiddleY = windowHeight / 2;
+
+      const rect = image.getBoundingClientRect();
+      const imageMiddleY = rect.top + rect.height / 2;
+
+      const distance = imageMiddleY - windowMiddleY;
+
+      let rotationAngle = (distance / windowHeight) * 15;
+
+      rotationAngle = Math.max(-15, Math.min(15, rotationAngle));
+
+      image.style.transform = `rotate(${rotationAngle}deg)`;
+    });
+  }
+
+  // const animContainres = document.querySelectorAll(".anim-container");
   let ellipseBtns = document.querySelectorAll(".ellipse-btn");
+
+  // animContainres.forEach((container) => {
+  //   const textCont = document.querySelector(".text-container");
+  //   container.style.height = textCont.offsetHeight + "px";
+  // });
 
   ellipseBtns.forEach(function (ellipseBtn) {
     let ellipseLink = ellipseBtn.nextElementSibling;
 
+    const fillEllipse = function (btn, link) {
+      btn.style.backgroundColor = "black";
+      btn.style.border = "none";
+      link.style.color = "white";
+    };
+
+    const clearEllipse = function (btn, link) {
+      btn.style.backgroundColor = "transparent";
+      btn.style.border = "0.5px solid black";
+      link.style.color = "black";
+    };
+
     ellipseBtn.addEventListener("mouseover", function () {
-      this.style.backgroundColor = "black";
-      this.style.border = "none";
-      ellipseLink.style.color = "white";
+      fillEllipse(ellipseBtn, ellipseLink);
     });
 
     ellipseBtn.addEventListener("mouseout", function () {
-      this.style.backgroundColor = "transparent";
-      this.style.border = "0.5px solid black";
-      ellipseLink.style.color = "black";
+      clearEllipse(ellipseBtn, ellipseLink);
     });
 
     ellipseLink.addEventListener("mouseover", function () {
-      ellipseBtn.style.backgroundColor = "black";
-      ellipseBtn.style.border = "none";
-      this.style.color = "white";
+      fillEllipse(ellipseBtn, ellipseLink);
     });
 
     ellipseLink.addEventListener("mouseout", function () {
-      ellipseBtn.style.backgroundColor = "transparent";
-      ellipseBtn.style.border = "0.5px solid black";
-      this.style.color = "black";
+      clearEllipse(ellipseBtn, ellipseLink);
     });
+  });
+
+  window.addEventListener("scroll", function () {
+    shiftPortfolio();
+    animateAbout();
+    animateElements();
   });
 });
 
@@ -39,10 +117,32 @@ window.addEventListener("DOMContentLoaded", function () {
   animateElements();
 });
 
-window.addEventListener("scroll", function () {
-  animateAbout();
-  animateElements();
-});
+function shiftPortfolio() {
+  const parallaxContainers = document.querySelectorAll(
+    ".parallax-portfolio-container"
+  );
+  parallaxContainers.forEach(function (container) {
+    const containerHeight = container.offsetHeight;
+
+    const windowHeight = window.innerHeight;
+
+    const containerScrollPosition = container.getBoundingClientRect().top;
+
+    const scrollPercentage =
+      1 -
+      (containerScrollPosition + containerHeight) /
+        (windowHeight + containerHeight);
+
+    const parallaxImage = container.querySelector(".parallax-portfolio");
+    const imageHeight = parallaxImage.offsetHeight;
+
+    const maxShiftDistance = containerHeight - imageHeight;
+
+    const shiftDistance = (1 - scrollPercentage) * maxShiftDistance;
+
+    parallaxImage.style.transform = `translateY(${shiftDistance}px)`;
+  });
+}
 
 function animateAbout() {
   const placeholdersAbout = document.querySelectorAll(".img-placeholder");
@@ -53,6 +153,7 @@ function animateAbout() {
     img.alt = "Design Kuhn";
     img.classList.add("img-fluid");
     img.classList.add("about-img");
+    img.classList.add("no-shadow");
     if (isInViewport(placeholder)) {
       img.classList.add("about-animate-grow");
       placeholder.parentNode.insertBefore(img, placeholder.nextSibling);
